5867cd91f3b66fe964826913842a099837a7ad1f
lianhai <lianhairen@u.nus.edu>
2025-06-07 15:08:36 +0800

    add custom peft folder

---
+++ peft/src/peft/tuners/stateft/config.py
@@ -0,0 +1,229 @@
+# Copyright 2024-present the HuggingFace Inc. team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Optional, Union, Literal
+
+from peft.config import PeftConfig
+from peft.utils import PeftType
+
+
+@dataclass
+class StateFTConfig(PeftConfig):
+    """
+    This is the configuration class to store the configuration of a [`StateFTModel`].
+
+    Args:
+        target_modules (`Optional[Union[List[str], str]]`):
+            The names of the modules to apply the adapter to. If this is specified, only the modules with the specified
+            names will be replaced. When passing a string, a regex match will be performed. When passing a list of
+            strings, either an exact match will be performed or it is checked if the name of the module ends with any
+            of the passed strings. If this is specified as 'all-linear', then all linear/Conv1D modules are chosen,
+            excluding the output layer. If this is not specified, modules will be chosen according to the model
+            architecture. If the architecture is not known, an error will be raised -- in this case, you should specify
+            the target modules manually.
+        exclude_modules (`Optional[Union[List[str], str]]`):
+            The names of the modules to not apply the adapter. When passing a string, a regex match will be performed.
+            When passing a list of strings, either an exact match will be performed or it is checked if the name of the
+            module ends with any of the passed strings.
+        init_weights (`bool` ):
+            Whether to perform initialization of adapter weights. This defaults to `True`. Use default initialization of pytorch. Passing `False` is discouraged.
+        layers_to_transform (`Union[List[int], int]`):
+            The layer indices to transform. If a list of ints is passed, it will apply the adapter to the layer indices
+            that are specified in this list. If a single integer is passed, it will apply the transformations on the
+            layer at this index.
+        layers_pattern (`Optional[Union[List[str], str]]`):
+            The layer pattern name, used only if `layers_to_transform` is different from `None`. This should target the
+            `nn.ModuleList` of the model, which is often called `'layers'` or `'h'`.
+    """
+    in_features: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": "The input features of the module to apply the adapter to. Now it cannot be determined automatically, so it should be specified manually."
+        }
+    )
+    out_features: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": "The output features of the module to apply the adapter to. Now it cannot be determined automatically, so it should be specified manually."
+        }
+    )
+    target_modules: Optional[Union[list[str], str]] = field(
+        default=None,
+        metadata={
+            "help": "List of module names or regex expression of the module names to replace with LoRA."
+            "For example, ['q', 'v'] or '.*decoder.*(SelfAttention|EncDecAttention).*(q|v)$' "
+            "This can also be a wildcard 'all-linear' which matches all linear/Conv1D layers except the output layer."
+        },
+    )
+    exclude_modules: Optional[Union[list[str], str]] = field(
+        default=None,
+        metadata={"help": "List of module names or regex expression of the module names to exclude from LoRA."},
+    )
+    init_weights: Union[bool | Literal["gaussian"]] = field(
+        default=True,
+        metadata={
+            "help": (
+                "Whether to initialize the weights of the LoRA layers with their default initialization. Can be True, False or 'lycoris'."
+                "Default is True. Don't change this setting to False, except if you know exactly what you're doing."
+            ),
+        },
+    )
+    layers_to_transform: Optional[Union[list[int], int]] = field(
+        default=None,
+        metadata={
+            "help": "The layer indexes to transform, is this argument is specified, PEFT will transform only the layers indexes that are specified inside this list. If a single integer is passed, PEFT will transform only the layer at this index."
+        },
+    )
+    layers_pattern: Optional[Union[list[str], str]] = field(
+        default=None,
+        metadata={
+            "help": "The layer pattern name, used only if `layers_to_transform` is different to None and if the layer pattern is not in the common layers pattern. "
+            "This should target the `nn.ModuleList` of the model, which is often called `'layers'` or `'h'`."
+        },
+    )
+    in_features_pattern: Optional[Union[list[str], str]] = field(
+        default=None,
+        metadata={
+            "help": "The mapping from layer names or regexp expression to ranks which are different from the default rank"
+            "specified by `in_features`. For example, `{'^model.decoder.layers.0.mlp.down_proj': 4096}`."
+        },
+    )
+    out_features_pattern: Optional[Union[list[str], str]] = field(
+        default=None,
+        metadata={
+            "help": "The mapping from layer names or regexp expression to alphas which are different from the default alpha"
+            "specified by `out_features`. For example, `{'^model.decoder.layers.0.mlp.up_proj': 4096}`."
+        },
+    )
+    modules_to_save: Optional[list[str]] = field(
+        default=None,
+        metadata={
+            "help": "List of modules apart from LoRA layers to be set as trainable and saved in the final checkpoint. "
+            "For example, in Sequence Classification or Token Classification tasks, "
+            "the final layer `classifier/score` are randomly initialized and as such need to be trainable and saved."
+        },
+    )
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.peft_type = PeftType.STATEFT
+        self.target_modules = (
+            set(self.target_modules) if isinstance(self.target_modules, list) else self.target_modules
+        )
+        self.exclude_modules = (
+            set(self.exclude_modules) if isinstance(self.exclude_modules, list) else self.exclude_modules
+        )
+        # if target_modules is a regex expression, then layers_to_transform should be None
+        if isinstance(self.target_modules, str) and self.layers_to_transform is not None:
+            raise ValueError("`layers_to_transform` cannot be used when `target_modules` is a str.")
+
+        # if target_modules is a regex expression, then layers_pattern should be None
+        if isinstance(self.target_modules, str) and self.layers_pattern is not None:
+            raise ValueError("`layers_pattern` cannot be used when `target_modules` is a str.")
+        # check for layers_to_transform and layers_pattern
+        if self.layers_pattern and not self.layers_to_transform:
+            raise ValueError("When `layers_pattern` is specified, `layers_to_transform` must also be specified. ")
+
+
+@dataclass
+class StateFTLoraConfig(StateFTConfig):
+    """
+    This is the configuration class to store the configuration of a [`StateFTModel`].
+
+    Args:
+        r (`int`):
+            LoRA rank.
+        lora_alpha (`int`):
+            The alpha parameter for LoRA scaling.
+        rank_dropout (`float`):
+            The dropout probability for rank dimension during training.
+        target_modules (`Optional[Union[List[str], str]]`):
+            The names of the modules to apply the adapter to. If this is specified, only the modules with the specified
+            names will be replaced. When passing a string, a regex match will be performed. When passing a list of
+            strings, either an exact match will be performed or it is checked if the name of the module ends with any
+            of the passed strings. If this is specified as 'all-linear', then all linear/Conv1D modules are chosen,
+            excluding the output layer. If this is not specified, modules will be chosen according to the model
+            architecture. If the architecture is not known, an error will be raised -- in this case, you should specify
+            the target modules manually.
+        exclude_modules (`Optional[Union[List[str], str]]`):
+            The names of the modules to not apply the adapter. When passing a string, a regex match will be performed.
+            When passing a list of strings, either an exact match will be performed or it is checked if the name of the
+            module ends with any of the passed strings.
+        init_weights (`bool` ):
+            Whether to perform initialization of adapter weights. This defaults to `True`. Use default initialization of pytorch. Passing `False` is discouraged.
+        layers_to_transform (`Union[List[int], int]`):
+            The layer indices to transform. If a list of ints is passed, it will apply the adapter to the layer indices
+            that are specified in this list. If a single integer is passed, it will apply the transformations on the
+            layer at this index.
+        layers_pattern (`Optional[Union[List[str], str]]`):
+            The layer pattern name, used only if `layers_to_transform` is different from `None`. This should target the
+            `nn.ModuleList` of the model, which is often called `'layers'` or `'h'`.
+        rank_pattern (`dict`):
+            The mapping from layer names or regexp expression to ranks which are different from the default rank
+            specified by `r`. For example, `{'^model.decoder.layers.0.encoder_attn.k_proj': 16}`.
+        alpha_pattern (`dict`):
+            The mapping from layer names or regexp expression to alphas which are different from the default alpha
+            specified by `alpha`. For example, `{'^model.decoder.layers.0.encoder_attn.k_proj': 16}`.
+    """
+
+    r: int = field(default=8, metadata={"help": "LoRA rank"})
+    lora_alpha: int = field(default=8, metadata={"help": "LoRA alpha"})
+    rank_pattern: Optional[dict] = field(
+        default_factory=dict,
+        metadata={
+            "help": (
+                "The mapping from layer names or regexp expression to ranks which are different from the default rank specified by `r`. "
+                "For example, `{'^model.decoder.layers.0.encoder_attn.k_proj': 16}`."
+            )
+        },
+    )
+    alpha_pattern: Optional[dict] = field(
+        default_factory=dict,
+        metadata={
+            "help": (
+                "The mapping from layer names or regexp expression to alphas which are different from the default alpha specified by `lora_alpha`. "
+                "For example, `{'^model.decoder.layers.0.encoder_attn.k_proj': 16}`."
+            )
+        },
+    )
+    modules_to_save: Optional[list[str]] = field(
+        default=None,
+        metadata={
+            "help": "List of modules apart from LoRA layers to be set as trainable and saved in the final checkpoint. "
+            "For example, in Sequence Classification or Token Classification tasks, "
+            "the final layer `classifier/score` are randomly initialized and as such need to be trainable and saved."
+        },
+    )
+    lora_bias: bool = field(
+        default=False,
+        metadata={
+            "help": (
+                "Whether to enable the bias term for the LoRA B parameter. Typically, this should be disabled. The "
+                "main use case for this is when the LoRA weights were extracted from fully fine-tuned parameters so "
+                "the bias of those parameters can be taken into account."
+            )
+        },
+    )
+
+    def __post_init__(self):
+        super().__post_init__()
+        if self.lora_bias:
+            if self.init_weights not in (True, False):
+                raise ValueError(
+                    f"The argument lora_bias=True is only supported with init_weights=True or False, got "
+                    f"init_weights={self.init_weights} instead."
+                )
---
+++ peft/src/peft/tuners/stateft/layer.py
@@ -0,0 +1,293 @@
+# Copyright 2024-present the HuggingFace Inc. team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import warnings
+from typing import Any, List, Optional, Union
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers.pytorch_utils import Conv1D
+from accelerate.utils.imports import is_xpu_available
+
+from peft.tuners.tuners_utils import BaseTunerLayer, check_adapters_to_merge
+from peft.tuners.lora.dora import DoraLinearLayer
+from peft.utils.other import transpose
+
+
+
+class StateFTLayer(BaseTunerLayer):
+    # All names of layers that may contain (trainable) adapter weights
+    adapter_layer_names = ("StateFT",)
+    # All names of other parameters that may contain adapter-related parameters
+    other_param_names = ()
+
+    def __init__(self, base_layer: nn.Module, **kwargs) -> None:
+        self.base_layer = base_layer
+        self.input_shape = None
+        self.output_shape = None
+        self.StateFT = None
+        # Mark the weight as unmerged
+        self._disable_adapters = False
+        self.kwargs = kwargs
+        self.merged = False # StateFT does not support merging
+
+        base_layer = self.get_base_layer()
+
+    def update_layer(self, adapter_name, *args, **kwargs):
+        raise NotImplementedError(
+            f"update_layer is not implemented for {self.__class__.__name__}. "
+            "Please implement it in the derived class."
+        )
+
+    def base_update_layer(self, adapter_name, adapter_layer, init_weights):
+        assert isinstance(adapter_layer, nn.Module), (
+            f"adapter_layer should be an instance of nn.Module, but got {type(adapter_layer)}"
+        )   
+        self.StateFT[adapter_name] = adapter_layer
+
+        if init_weights:
+            self.reset_parameters(adapter_name)
+
+        self._move_adapter_to_device_of_base_layer(adapter_name)
+        self.set_adapter(self.active_adapters)
+
+    @torch.no_grad()
+    def reset_parameters(self, adapter_name):
+        raise NotImplementedError(
+            f"reset_parameters is not implemented for {self.__class__.__name__}. "
+            "Please implement it in the derived class."
+        )
+    
+    def forward(self, x: torch.Tensor, *args: Any, **kwargs: Any) -> torch.Tensor:
+        previous_dtype = x.dtype
+
+        if self.disable_adapters:
+            result = self.base_layer(x, *args, **kwargs)
+        else:
+            result = self.base_layer(x, *args, **kwargs)
+            for active_adapter in self.active_adapters:
+                if active_adapter not in self.StateFT.keys():
+                    continue
+                result = result + self.StateFT[active_adapter](x, *args, **kwargs).to(result.dtype)
+
+        result = result.to(previous_dtype)
+        return result
+
+
+class LoRAsideLayer(nn.Module):
+    """
+    A side layer for LoRA-like layers that contains the LoRA parameters.
+    This is used to separate the LoRA parameters from the base layer parameters.
+    """
+
+    def __init__(self, in_features: int, out_features: int, r: int,
+            lora_alpha: float = 1.0, lora_dropout: float = 0.0,
+            init_lora_weights: bool = True,
+            use_rslora: bool = False,
+            lora_bias: bool = False):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.update_layer(
+            r=r,
+            lora_alpha=lora_alpha,
+            lora_dropout=lora_dropout,
+            init_lora_weights=init_lora_weights,
+            use_rslora=use_rslora,
+            lora_bias=lora_bias,
+        )
+    def update_layer(
+        self,
+        r,
+        lora_alpha,
+        lora_dropout,
+        init_lora_weights,
+        use_rslora,
+        lora_bias: bool = False,
+    ):
+        # This code works for linear layers, override for other layer types
+        if r <= 0:
+            raise ValueError(f"`r` should be a positive integer value but the value passed is {r}")
+
+        self.r = r
+        self.lora_alpha = lora_alpha
+        if lora_dropout > 0.0:
+            lora_dropout_layer = nn.Dropout(p=lora_dropout)
+        else:
+            lora_dropout_layer = nn.Identity()
+
+        self.lora_dropout=lora_dropout_layer
+        # Actual trainable parameters
+        self.lora_A = nn.Linear(self.in_features, r, bias=False)
+        self.lora_B = nn.Linear(r, self.out_features, bias=lora_bias)
+        self.lora_bias = lora_bias
+
+        if use_rslora:
+            self.scaling = lora_alpha / math.sqrt(r)
+        else:
+            self.scaling = lora_alpha / r
+
+        # for inits that require access to the base weight, use gather_param_ctx so that the weight is gathered when using DeepSpeed
+        
+        if init_lora_weights == "eva":
+            nn.init.zeros_(self.lora_B.weight)
+        elif init_lora_weights:
+            self.reset_lora_parameters(init_lora_weights)
+
+    def reset_lora_parameters(self, init_lora_weights):
+        if init_lora_weights is False:
+            return
+
+        if init_lora_weights is True:
+            # initialize A the same way as the default for nn.Linear and B to zero
+            # https://github.com/microsoft/LoRA/blob/a0a92e0f26c067cf94747bdbf1ce73793fa44d19/loralib/layers.py#L124
+            nn.init.kaiming_uniform_(self.lora_A.weight, a=math.sqrt(5))
+        elif init_lora_weights.lower() == "gaussian":
+            nn.init.normal_(self.lora_A.weight, std=1 / self.r)
+        else:
+            raise ValueError(f"Unknown initialization {init_lora_weights=}")
+        nn.init.zeros_(self.lora_B.weight)
+        if self.lora_bias:
+            nn.init.zeros_(self.lora_B.bias)
+
+    def get_delta_weight(self, adapter) -> torch.Tensor:
+        """
+        Compute the delta weight for the given adapter.
+
+        Args:
+            adapter (str):
+                The name of the adapter for which the delta weight should be computed.
+        """
+        device = self.lora_B.weight.device
+        dtype = self.lora_B.weight.dtype
+
+        # In case users wants to merge the adapter weights that are in
+        # float16 while being on CPU, we need to cast the weights to float32, perform the merge and then cast back to
+        # float16 because the `@` and matmul operation in general is not supported in torch + cpu + fp16.
+        cast_to_fp32 = device.type == "cpu" and dtype == torch.float16
+
+        weight_A = self.lora_A.weight
+        weight_B = self.lora_B.weight
+
+        if cast_to_fp32:
+            weight_A = weight_A.float()
+            weight_B = weight_B.float()
+
+        output_tensor = (weight_B @ weight_A) * self.scaling
+
+        if cast_to_fp32:
+            output_tensor = output_tensor.to(dtype=dtype)
+
+            # cast back the weights
+            self.lora_A.weight.data = weight_A.to(dtype)
+            self.lora_B.weight.data = weight_B.to(dtype)
+
+        return output_tensor
+    
+    def forward(self, x: torch.Tensor, *args: Any, **kwargs: Any) -> torch.Tensor:
+        lora_A = self.lora_A
+        lora_B = self.lora_B
+        dropout = self.lora_dropout
+        scaling = self.scaling
+        result = lora_B(lora_A(dropout(x.to(lora_A.weight.dtype)))) * scaling
+
+        return result
+
+class StateFTLoraLayer(nn.Module, StateFTLayer):
+    # StateFT implemented as a LoRA-like layer
+
+    def __init__(
+        self,
+        base_layer,
+        adapter_name: str,
+        in_features,
+        out_features,
+        r,
+        lora_alpha,
+        lora_dropout,
+        init_weights,
+        use_rslora,
+        lora_bias: bool = False,
+        **kwargs,
+    ) -> None:
+        assert not isinstance(base_layer, nn.Embedding), 'StateFTLora does not support embedding layers.' 
+        super().__init__()
+        StateFTLayer.__init__(self, base_layer, **kwargs)
+        self._active_adapter = adapter_name
+        self.kwargs = kwargs
+
+        self.update_layer(
+            adapter_name, 
+            in_features=in_features,
+            out_features=out_features,
+            r=r,
+            lora_alpha=lora_alpha,
+            lora_dropout=lora_dropout,
+            init_lora_weights=init_weights,
+            use_rslora=use_rslora,
+            lora_bias=lora_bias,
+        )
+
+    def update_layer(
+        self,
+        adapter_name,
+        in_features,
+        out_features,
+        r,
+        lora_alpha,
+        lora_dropout,
+        init_weights,
+        use_rslora,
+        lora_bias: bool = False,):
+        # This code works for linear layers, override for other layer types
+        lora_layer= LoRAsideLayer(
+            in_features=in_features,
+            out_features=out_features,
+            r=r,
+            lora_alpha=lora_alpha,
+            lora_dropout=lora_dropout,
+            init_lora_weights=init_weights,
+            use_rslora=use_rslora,
+            lora_bias=lora_bias,
+        )
+        super().base_update_layer(adapter_name, lora_layer, False)
+
+    def merge(self, safe_merge: bool = False, adapter_names: Optional[List[str]] = None) -> None:
+        """
+        Merge the active adapter weights into the base weights
+
+        Args:
+            safe_merge (`bool`, *optional*):
+                If True, the merge operation will be performed in a copy of the original weights and check for NaNs
+                before merging the weights. This is useful if you want to check if the merge operation will produce
+                NaNs. Defaults to `False`.
+            adapter_names (`List[str]`, *optional*):
+                The list of adapter names that should be merged. If None, all active adapters will be merged. Defaults
+                to `None`.
+        """
+        raise ValueError("StateFTLora does not support merging")
+
+    def unmerge(self) -> None:
+        """
+        This method unmerges all merged adapter layers from the base weights.
+        """
+        raise ValueError("StateFTLora does not support merging")
+
+    
+
+    def __repr__(self) -> str:
+        rep = super().__repr__()
+        return "StateFT." + rep
